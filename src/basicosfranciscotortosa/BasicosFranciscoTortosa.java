f910ba19614bc86ffe307b8105c24727c7c718a5
DESKTOP-GL52QQN\Francisco <rerustraven@gmail.com>
2018-10-27 20:22:41 +0200

    Logicos

--- src/basicosfranciscotortosa/BasicosFranciscoTortosa.java
+++ src/basicosfranciscotortosa/BasicosFranciscoTortosa.java
@@ -16,6 +16,18 @@ public class BasicosFranciscoTortosa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        logicos();
     }
-    
+      public static void logicos() {
+          //Variables
+          int x = 3;
+          int y = 8;
+          System.out.println("El valor de X es 3 y el de Y es 8");
+          if (y > x) {
+              System.out.println("Y es mayor que X");                      
+          }
+          if (x > y) {
+              System.out.println("Y es menor que X");
+          }
+    }    
 }
